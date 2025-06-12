0d7bb97b2217567fb1ed4a2f0a96640f4c6b4d3c
Andre Yuhji Terada <andre.terada@ifood.com.br>
2025-06-12 01:30:09 -0300

    Initial commit

---
+++ src/main/java/net/jty/chiikawacraft/ChiikawaCraftClient.java
@@ -0,0 +1,10 @@
+package net.jty.chiikawacraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChiikawaCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
